99aee9d870d3d1bfc7496987340ba7784c735fb2
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2020-10-20 22:03:54 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,48 +1,85 @@
 const DOMSelectors = {
   displayContainer: document.querySelector(".container"),
   startButton: document.querySelector(".start-btn"),
+  retryButton: document.querySelector(".retry-btn"),
 };
-submitButton = "";
+
 const questions = [
   {
     number: 0,
-    question: "(Placeholder) Is Africa a Country?",
-    choiceOne: "Yes",
-    choiceTwo: "No",
-    choiceThree: "Maybe",
-    choiceFour: "It's a planet",
-    correctAnswer: 1,
+    question: "Who is the only US President to have served more than two Terms?",
+    choiceOne: "FDR",
+    choiceTwo: "LBJ",
+    choiceThree: "George Washington",
+    choiceFour: "JFK",
+    correctAnswer: 0,
   },
   {
     number: 1,
-    question: "(Placeholder) Who is the President of the United States?",
-    choiceOne: "Yes",
-    choiceTwo: "Politics is the mind killer",
-    choiceThree: "Hillary Clinton",
-    choiceFour: "Karl Marx",
+    question: "In what year did Hitler become Chanceller of Germany?",
+    choiceOne: "1917",
+    choiceTwo: "1933",
+    choiceThree: "1939",
+    choiceFour: "1945",
     correctAnswer: 1,
   },
   {
     number: 2,
-    question: "Neal Stephenson?",
-    choiceOne: "Author",
-    choiceTwo: "Politician",
-    choiceThree: "Scientist",
-    choiceFour: "Historian",
+    question: "Which of the following leaders was NOT elected",
+    choiceOne: "Hitler",
+    choiceTwo: "Mousollini",
+    choiceThree: "Stalin",
+    choiceFour: "Franklin D. ROosevelt",
+    correctAnswer: 2,
+  },
+  {
+    number: 3,
+    question: "What was the policy taken by Great Britain and France in response to Hitler's advances the 1930's known as?",
+    choiceOne: "Imperialism",
+    choiceTwo: "Globalism",
+    choiceThree: "Mercantilism",
+    choiceFour: "Appeasement",
+    correctAnswer: 3,
+  },
+  {
+    number: 4,
+    question: "What event caused the US to enter the Second World War?",
+    choiceOne: "Pearl Harbour",
+    choiceTwo: "Zimmerman Telegram",
+    choiceThree: "Hindenburg Explosion",
+    choiceFour: "D-Day",
     correctAnswer: 0,
   },
+  {
+    number: 5,
+    question: "What demilitarized zone did Hitler fortify in the months leading up the war?",
+    choiceOne: "Sudetenland",
+    choiceTwo: "Rhineland",
+    choiceThree: "Nuremburg",
+    choiceFour: "Normandy",
+    correctAnswer: 1,
+  },
+  {
+    number: 6,
+    question: "What unfair treaty paved the way for Hitler's rise to power and the Nazi popularity?",
+    choiceOne: "Conference of Berlin",
+    choiceTwo: "The Treaty of Paris",
+    choiceThree: "The Treaty of Versailles",
+    choiceFour: "The Conference of Rome",
+    correctAnswer: 2,
+  }
 ];
 
-let questionNumber = 0;
-let correctAnswers = 0;
-
-const init = function () {
+(function init() {
+  submitButton = "";
+  questionNumber = 0;
+  correctAnswers = 0;
   DOMSelectors.startButton.addEventListener("click", function (e) {
     DOMSelectors.displayContainer.innerHTML = "";
     DOMSelectors.displayContainer.insertAdjacentHTML(
       "afterbegin",
       `<ul class="question_container">
-        <h1>${correctAnswers}/${questions.length}</h1>
+        <h1>Correct:${correctAnswers}/${questions.length}</h1>
         <h1>${questions[questionNumber].question}</h1>
         <input type="button" class="choice" id="choice-0" value="${questions[questionNumber].choiceOne}">
         <input type="button" class="choice" id="choice-1" value="${questions[questionNumber].choiceTwo}">
@@ -77,15 +114,20 @@ const init = function () {
     }
   });
   document.addEventListener("click", function (e) {
+    if (e.target.id == "retry") {
+      location.reload();
+    }
+  });
+  document.addEventListener("click", function (e) {
     if (e.target.className == "submit-btn btn") {
       console.log("The submit button was pressed.");
       if (typeof answer == "undefined") {
+        alert("YOU MUST SELECT AN ANSWER! DO IT!!!");
         console.log("no answer has been selected");
       } else {
         console.log(`Answer ${answer} was selected.`);
         if (answer == questions[questionNumber].correctAnswer) {
           correctAnswers++;
-
         }
         if (questionNumber < questions.length - 1) {
           questionNumber++;
@@ -94,7 +136,7 @@ const init = function () {
           DOMSelectors.displayContainer.insertAdjacentHTML(
             "afterbegin",
             `<ul class="question_container">
-            <h1>${correctAnswers}/${questions.length}</h1>
+              <h1>${correctAnswers}/${questions.length}</h1>
               <h1>${questions[questionNumber].question}</h1>
               <input type="button" class="choice" id="choice-0" value="${questions[questionNumber].choiceOne}">
               <input type="button" class="choice" id="choice-1" value="${questions[questionNumber].choiceTwo}">
@@ -108,13 +150,14 @@ const init = function () {
           DOMSelectors.displayContainer.innerHTML = "";
           DOMSelectors.displayContainer.insertAdjacentHTML(
             "afterbegin",
-            `<h1>The quiz is now over.</h1>
-            <h1>${correctAnswers}/${questions.length}</h1>`
+            `<h1>You have Completed the WW2 Quiz!</h1>
+            <h1>You got ${correctAnswers} out of ${questions.length} questions correct!</h1>
+            <input type="button" class="retry-btn btn" id="retry" value="Try Again?">`
           );
         }
       }
     }
   });
-};
+})();
 
-init();
+
